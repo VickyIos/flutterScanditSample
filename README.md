b74eabe09ac2eb2b1bfe86d7221527acce5cc026
Vignesh <mailmevignesh7@gmail.com>
2024-07-01 20:19:28 +0530

    - Files added.

--- README.md
+++ README.md
@@ -1,32 +1,8 @@
-# flutter_module_sample
+# flutter_module_sam
 
 A new Flutter module project.
 
 ## Getting Started
-
-Steps:
-
-1.Build a new AAR file by `flutter build aar` from this Flutter Module Project
-
-2.Copy the maven URL from the terminal after the generation of aar file
-
-```
-maven {
-            url '../build/host/outputs/repo'
-      }
-
-```
-
-3.Copy dependencies from the terminal after the generation of aar file
-
-```
-dependencies {
-      debugImplementation 'com.example.flutter_module_sam:flutter_debug:1.0'
-      profileImplementation 'com.example.flutter_module_sam:flutter_profile:1.0'
-      releaseImplementation 'com.example.flutter_module_sam:flutter_release:1.0'
-    }
-```
-
 
 For help getting started with Flutter development, view the online
 [documentation](https://flutter.dev/).
---
+++ lib/methodchannel_login_helper.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class MethodChannelService {
+  final _channel = const MethodChannel('com.scandit.fluttermodule');
+  static final MethodChannelService _instance = MethodChannelService._();
+  Function(String)? _loginCallBack;
+  Function(String)? _homeCallBack;
+
+  factory MethodChannelService() {
+    return _instance;
+  }
+
+  MethodChannelService._() {
+    initialize();
+  }
+
+  void initialize() {
+    _channel.setMethodCallHandler(_handleMessage);
+  }
+
+  Future<dynamic> _handleMessage(MethodCall call) async {
+    debugPrint("Method channel call in helper : $call $_loginCallBack $_homeCallBack");
+    if (call.method == 'scandit_rfxcel') {
+      String response = call.arguments as String;
+      if (_loginCallBack != null) {
+        _loginCallBack!(response);
+      } else if (_homeCallBack != null) {
+        _homeCallBack!(response);
+      }
+    }
+  }
+
+  set setLoginCallBack(Function(String)? funObj) {
+    _loginCallBack = funObj;
+  }
+
+  set setHomeCallBack(Function(String)? funObj) {
+    _homeCallBack = funObj;
+  }
+}
